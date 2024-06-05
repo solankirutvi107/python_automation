fe9e65da289f8b0ac7482b3ee5f38a0bc607acaf
Mukesh Kumar <mukesh.k@eno8.com>
2024-06-05 08:18:40 +0530

    "variable and data type"

---
+++ ex_05062024/day2.py
@@ -0,0 +1,10 @@
+print("Hello World")
+# self -- concept in OOps which points to itself
+# *args -- unlimited number of arguments * -string,boolean,int ..
+# sep=' ' --how you want to seperate the argument
+# end='\n' --(default value is \n) .in the end what you want to do
+#sep,end,file command should be last in the print
+# file=None -- File IO
+
+print("Hello", "world", 112, True, 3.14,sep="-",end="**")
+print("hey")
---
+++ ex_05062024/exe.py
@@ -0,0 +1,10 @@
+# Program to find  the  max in two number
+print(max(10, 29))
+print(max(23, 88, 43, 54))
+print(max(23, -1, -5, 55, -56))
+
+#TypeError: '>' not supported between instances of 'str' and 'int'
+print(max(23,65,"Rut"))
+
+#IndentationError: unexpected indent
+   print("rut")
